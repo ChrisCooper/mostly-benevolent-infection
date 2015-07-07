bf13734297025b14b144f5ac18051e1bacd87023
Chris Cooper <chris@quixotic-chris.com>
2015-07-07 16:58:08 -0400

    made tests more concise with setUp()

--- tests/test_models.py
+++ tests/test_models.py
@@ -2,32 +2,30 @@ import unittest
 import models
 
 class TestUserModelBasics(unittest.TestCase):
+    def setUp(self):
+        self.u = models.User()
+
     def test_new_user_has_correct_values(self):
-        u = models.User()
-        self.assertTrue(len(u.email) > 0, msg='no email is set')
-        self.assertEqual(u.site_version, 1)
-        self.assertEqual(len(u.coach_set), 0)
-        self.assertEqual(len(u.learner_set), 0)
+        self.assertTrue(len(self.u.email) > 0, msg='no email is set')
+        self.assertEqual(self.u.site_version, 1)
+        self.assertEqual(len(self.u.coach_set), 0)
+        self.assertEqual(len(self.u.learner_set), 0)
 
     def test_coach_must_be_a_user(self):
-        u = models.User()
-        self.assertRaises(AttributeError, lambda: self.add_bad_coach(u))
+        self.assertRaises(AttributeError, lambda: self.add_bad_coach(self.u))
 
     def add_bad_coach(self, user):
         user.add_coach('i\'m a board certified spanish tutor')
 
     def test_coach_relationship_works(self):
-        u = models.User()
         c = models.User()
-        u.add_coach(c)
+        self.u.add_coach(c)
 
-        self.assertTrue(c in u.coach_set)
-        self.assertTrue(u in c.learner_set)
+        self.assertTrue(c in self.u.coach_set)
+        self.assertTrue(self.u in c.learner_set)
 
     def test_site_versions_must_be_ints(self):
-        u = models.User()
-
-        self.assertRaises(TypeError, lambda: self.assign_bad_site_version(u))
+        self.assertRaises(TypeError, lambda: self.assign_bad_site_version(self.u))
 
     def assign_bad_site_version(self, user):
         user.site_version = 'bad'
