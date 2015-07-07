8b8b7643e28e5b37b9d5e926d13243fdacefea37
Chris Cooper <chris@quixotic-chris.com>
2015-07-07 16:02:01 -0400

    added stub tests for infection error handling

---
+++ infection.py
@@ -0,0 +1,8 @@
+"""
+The proper infection algorithms are implemented here.
+
+This file is the best tested and the most robust.
+"""
+
+def infect(user, new_site_version):
+    pass
---
+++ tests/test_infection.py
@@ -0,0 +1,16 @@
+import unittest
+import models
+
+
+class TestInfectionErrors(unittest.TestCase):
+    def test_user_must_be_valid(self):
+        # Not none
+        # User instance
+        pass
+
+    def test_version_must_be_int(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
