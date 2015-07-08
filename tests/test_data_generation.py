b8bbdb04e8a2dda2eb0b9a3b546dd573bc9c3b99
Chris Cooper <chris@quixotic-chris.com>
2015-07-07 20:33:56 -0400

    removed reliance on numpy. for mean? really?

--- tests/test_data_generation.py
+++ tests/test_data_generation.py
@@ -33,13 +33,13 @@ class TestClassroomGeneration(unittest.TestCase):
             for c in range(n)
         ]
 
-        mean_users = numpy.mean([len(c) for c in classrooms])
+        mean_users = mean([len(c) for c in classrooms])
 
         # We generate the 99.99% confidence interval for this sample mean
         max_distance = z_for_99_point_99_confidence * pop_std_dev / numpy.sqrt(n)
 
         self.assertTrue(
-            numpy.abs(mean_users - pop_avg) < max_distance,
+            abs(mean_users - pop_avg) < max_distance,
             msg='classroom size exceeded expected bounds (may happen 1 out of 10,000 times)'
         )
 
@@ -129,6 +129,12 @@ class TestFunEmails(unittest.TestCase):
             emails_set.add(fun.cool_email())
 
         self.assertEqual(len(emails_set), num_trials, msg="emails were not unique")
+
+
+def mean(nums):
+    return sum(nums) * 1.0 / len(nums)
+
+
 
 
 if __name__ == '__main__':
